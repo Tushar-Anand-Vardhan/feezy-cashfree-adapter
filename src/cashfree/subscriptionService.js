ffd2cb7626045270375c8ada171413d02310d5ea
Tushar Vardhan <tushar220.tv@gmail.com>
2026-01-23 17:42:05 +0530

    test

--- src/cashfree/subscriptionService.js
+++ src/cashfree/subscriptionService.js
@@ -9,9 +9,30 @@ async function cashfreePartnerPost(path, body = {}, extraHeaders = {}) {
     'x-api-version': '2025-01-01',
     ...extraHeaders 
   };
+  console.log('[CF REQUEST]', {
+    method: 'POST',
+    baseURL: axiosInstance.defaults.baseURL,
+    path,
+    headers: {
+      'x-partner-apikey': PARTNER_KEY ? 'SET' : 'MISSING',
+      'x-api-version': extraHeaders['x-api-version'],
+      'content-type': extraHeaders['Content-Type']
+    },
+    body
+  });
+  
   const url = path.startsWith('http') ? path : `${path.startsWith('/') ? '' : '/'}${path}`;
-  const resp = await axiosInstance.post(url, body, { headers });
-  return resp.data;
+  try {
+    const resp = await axiosInstance.post(url, body, { headers });
+    return resp.data;
+  } catch (err) {
+    console.error('[CF ERROR]', {
+      status: err.response?.status,
+      data: err.response?.data,
+      headers: err.response?.headers
+    });
+    throw err;
+  }
 }
 
 // create subscription using PG endpoint (not partners base)
